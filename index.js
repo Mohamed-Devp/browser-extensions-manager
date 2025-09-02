528a74074eeae8decb87d29bdb1b76d85811c76c
Mohamed-Devp <Mohamed-Devp@users.noreply.github.com>
2025-09-02 21:12:11 +0000

    deploy: 1356af963974699de3cd26febc8367478fd6d88e

---
+++ index.js
@@ -0,0 +1,120 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const extensionsGrid = document.querySelector('.extensions-grid');
+const filters = Array.from(document.querySelectorAll('.filter'));
+let extensions = [];
+let activeFilter = 0;
+function loadJson(url) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const response = yield fetch(url);
+            if (!response.ok) {
+                throw new Error(`Failed to fetch ${url}. ${response.statusText}`);
+            }
+            return response.json();
+        }
+        catch (error) {
+            console.error(error);
+        }
+    });
+}
+function updateExtensions() {
+    if (!(extensionsGrid instanceof HTMLElement)) {
+        return;
+    }
+    let cardsHtml = '';
+    // Filter the extensions based on the active filter
+    const filteredExtensions = extensions.filter(extension => {
+        return activeFilter === 0 || (activeFilter === 1 && extension.isActive) || (activeFilter === 2 && !extension.isActive);
+    });
+    filteredExtensions.forEach((extension, key) => {
+        cardsHtml += `
+            <div class="extension" data-name="${extension.name}">
+				<div class="info">
+					<img src="${extension.logo}" alt="${extension.name}">
+					<div>
+						<p class="name">${extension.name}</p>
+						<p class="description">${extension.description}</p>
+					</div>
+				</div>
+				<div class="actions">
+					<button class="remove">Remove</button>
+					<div class="switch">
+						<label for="toggle-${key}">
+							<div class="toggle"></div>
+						</label>
+						<input type="checkbox"  id="toggle-${key}" ${extension.isActive ? "checked" : ""}>
+					</div>
+				</div>
+			</div>
+        `;
+    });
+    // Update the extensions grid
+    extensionsGrid.innerHTML = cardsHtml;
+    // Update the event listeners
+    const activeToggles = extensionsGrid.querySelectorAll('input[type="checkbox"]');
+    const removeButtons = extensionsGrid.querySelectorAll('.remove');
+    activeToggles.forEach(toggle => {
+        toggle.addEventListener('change', toggleActive);
+    });
+    removeButtons.forEach(button => {
+        button.addEventListener('click', removeExtension);
+    });
+}
+function updateFilter(newIndex) {
+    // Remove "active" from the currently active filter, if any
+    const current = filters[activeFilter];
+    if (current) {
+        current.classList.remove("active");
+    }
+    // Add "active" to the new filter
+    const next = filters[newIndex];
+    if (next) {
+        next.classList.add("active");
+    }
+    // Update the active index
+    activeFilter = newIndex;
+    updateExtensions();
+}
+function toggleActive(_) {
+    const target = this.closest('.extension');
+    const name = (target instanceof HTMLElement) ? target.dataset.name : '';
+    // Toggle between active and inactive
+    extensions = extensions.map(extension => {
+        if (extension.name === name) {
+            extension.isActive = !extension.isActive;
+        }
+        return extension;
+    });
+    // Update the extensions view if the active filter is not 'all'
+    if (activeFilter != 0) {
+        setTimeout(() => {
+            updateExtensions();
+        }, 200);
+    }
+}
+function removeExtension(_) {
+    const target = this.closest('.extension');
+    const name = (target instanceof HTMLElement) ? target.dataset.name : '';
+    extensions = extensions.filter(exension => exension.name !== name);
+    updateExtensions();
+}
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        extensions = yield loadJson('../extensions.json');
+        updateExtensions();
+        filters.forEach((filter, key) => {
+            filter.addEventListener('click', () => updateFilter(key));
+        });
+    });
+}
+main();
+export {};
+//# sourceMappingURL=index.js.map
